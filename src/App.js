159cfa5befcf3f3995c795b8fbcb0de3009e464e
Max Mynter <maxmynter@me.com>
2022-12-25 22:15:00 +0100

    Ex. 5.3

--- src/App.js
+++ src/App.js
@@ -10,26 +10,34 @@ const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      //noteService.setToken(user.token);
+    }
+  }, []);
+
   return (
     <div>
       {errorMessage ? <DisplayMessage message={errorMessage} /> : null}
-      {user ? (
-        <BlogsView blogs={blogs} />
-      ) : (
-        <LoginForm
-          username={username}
-          setUsername={setUsername}
-          password={password}
-          setPassword={setPassword}
-          setUser={setUser}
-          setErrorMessage={setErrorMessage}
-        />
-      )}
+      {user && <BlogsView blogs={blogs} />}
+      <LoginForm
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+        user={user}
+        setUser={setUser}
+        setErrorMessage={setErrorMessage}
+      />
     </div>
   );
 };
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,6 +5,7 @@ const LoginForm = ({
   setUsername,
   password,
   setPassword,
+  user,
   setUser,
   setErrorMessage,
 }) => {
@@ -13,6 +14,7 @@ const LoginForm = ({
 
     try {
       const user = await loginService.login({ username, password });
+      window.localStorage.setItem("loggedBlogAppUser", JSON.stringify(user));
       setUser(user);
       setUsername("");
       setPassword("");
@@ -23,29 +25,42 @@ const LoginForm = ({
       }, 5000);
     }
   };
-  return (
-    <form onSubmit={handleLogin}>
-      <div>
-        Username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        Password
-        <input
-          type="text"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  );
+
+  const handleLogout = () => {
+    window.localStorage.removeItem("loggedBlogAppUser");
+    setUser(null);
+  };
+  if (user) {
+    return (
+      <>
+        <button onClick={handleLogout}>Logout</button>
+      </>
+    );
+  } else {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          Username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          Password
+          <input
+            type="text"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    );
+  }
 };
 
 export default LoginForm;
