80b38a41518fc3f08f55ad9344d4da623ed954ff
Max Mynter <maxmynter@me.com>
2022-12-27 20:41:22 +0100

    Ex. 5.18 Bonus

--- cypress/e2e/loggingIn.cy.js
+++ cypress/e2e/loggingIn.cy.js
@@ -11,6 +11,14 @@ describe("Blog app", function () {
     cy.contains("Wrong credentials");
     cy.contains("Login");
   });
+
+  it("Shows wrong login message in red", function () {
+    cy.get("#usernameInput").type("Max");
+    cy.get("#passwordInput").type("212");
+    cy.contains("Login").click();
+    cy.get(".message").should("have.css", "color", "rgb(255, 0, 0)");
+  });
+
   it("Can log in with correct credentials", function () {
     cy.get("#usernameInput").type("Max");
     cy.get("#passwordInput").type("123");
