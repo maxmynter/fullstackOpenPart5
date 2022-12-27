e53326ada9b05f17547bbc272cb8e5eb721b30a9
Max Mynter <maxmynter@me.com>
2022-12-27 13:06:31 +0100

    Ex. 5.14

--- src/components/BlogView.test.js
+++ src/components/BlogView.test.js
@@ -1,27 +1,42 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Blog } from "./BlogsView";
 
-/*
-Make a test which checks that the component displaying a blog renders the blog's title and author,
-but does not render its url or number of likes by default.
-
-Add CSS-classes to the component to help the testing as necessary.
-*/
+const blogEntry = {
+  title: "Fancy Title",
+  author: "Me",
+  url: "abc.de",
+  likes: 0,
+  creator: {
+    name: "Not Me",
+  },
+};
 
 test("Blog component renders title and author", () => {
-  const blogEntry = {
-    title: "Fancy Title",
-    author: "Me",
-    url: "abc.de",
-    creator: {
-      name: "Not Me",
-    },
-  };
   render(<Blog blog={blogEntry} />);
   const titleText = screen.findByText("Fancy Title");
   const authorText = screen.findByText("Me");
   expect(titleText).toBeDefined();
   expect(authorText).toBeDefined();
+});
+
+test("Blog url and number of likes are not shown after when View Details is not pressed", async () => {
+  render(<Blog blog={blogEntry} />);
+
+  expect(await screen.queryByText("abc.de")).toBeNull();
+  expect(await screen.queryByText("Likes")).toBeNull();
+});
+
+test("Blog url and number of likes are only shown after View Details is pressed", async () => {
+  const user = userEvent.setup();
+  render(<Blog blog={blogEntry} />);
+
+  const button = screen.getByText("View Details");
+  await user.click(button);
+  let titleURL = await screen.queryByText("abc.de");
+  let likes = await screen.queryByText("Likes");
+  expect(titleURL).toBeDefined();
+  expect(likes).toBeDefined();
 });
